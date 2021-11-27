62f17aea2723f9f2f7ea33824aa0f6abfb2c5865
Lovro Habjan <lh5107@student.uni-lj.si>
2021-11-27 16:31:51 +0100

    Add more initial users to db.

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,1 +1,3 @@
-INSERT INTO users (firstname, lastname, email) VALUES ('John', 'Doe', 'john.doe@mail.com')
+INSERT INTO users (firstname, lastname, email) VALUES ('John', 'Doe', 'john.doe@mail.com');
+INSERT INTO users (firstname, lastname, email) VALUES ('Allan', 'Smith', 'allan.smith@mail.com');
+INSERT INTO users (firstname, lastname, email) VALUES ('Jane', 'Smith', 'jane.smith@mail.com');
