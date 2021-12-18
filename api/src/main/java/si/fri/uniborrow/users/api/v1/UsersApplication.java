b55a1fd965f4302df93e3c2b288e2325519582bb
Lovro Habjan <lh5107@student.uni-lj.si>
2021-12-18 18:18:04 +0100

    Add support for Consul service discovery
    Added service discovery for Consul, using the "dev" environment and
    the name "uniborrow-users-service", to be consistent with other code
    base.

--- api/src/main/java/si/fri/uniborrow/users/api/v1/UsersApplication.java
+++ api/src/main/java/si/fri/uniborrow/users/api/v1/UsersApplication.java
@@ -1,8 +1,11 @@
 package si.fri.uniborrow.users.api.v1;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@RegisterService(value = "uniborrow-users-service", environment = "dev", version = "1.0.0")
 @ApplicationPath("/v1")
 public class UsersApplication extends Application {
 }
